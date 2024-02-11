9591b2321397b82864fdea27262ef54edaf56f32
Anna4voit <137306965+Anna4voit@users.noreply.github.com>
2024-02-11 23:22:01 +0200

    update-1
    used controlled input

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
 import { useDispatch } from 'react-redux';
 import { addContact } from '../../redux/contacts-operations';
 
@@ -6,28 +8,56 @@ import css from './ContactForm.module.css';
 export const ContactForm = () => {
   const dispatch = useDispatch();
 
+  const nameId = nanoid();
+  const phoneId = nanoid();
+
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleChange = event => {
+    const { name, value } = event.currentTarget;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
+  };
+
   const handleSubmit = event => {
     event.preventDefault();
-    const { name, number } = event.target.elements;
-    const data = { name: name.value, number: number.value };
+
+    const data = { name, number };
     dispatch(addContact(data));
-    event.target.reset();
+
+    setName('');
+    setNumber('');
   };
 
   return (
     <form className={css.form} onSubmit={handleSubmit}>
-      <label>Name</label>
+      <label htmlFor={nameId}>Name</label>
       <input
         type="text"
         name="name"
+        value={name}
+        onChange={handleChange}
+        id={nameId}
         pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         required
       />
 
-      <label>Number</label>
+      <label htmlFor={phoneId}>Number</label>
       <input
         type="tel"
         name="number"
+        value={number}
+        onChange={handleChange}
+        id={phoneId}
         pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
         required
       />
